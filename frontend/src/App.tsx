999dd86e7ffaaaa466c0d0cb2aafcc8e03917258
Douglas Henrique <dev.dhbarboza@gmail.com>
2024-06-03 22:54:25 -0300

    Update Tailwind config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
     return (
         <>
-            <h1 className="bg-gray-900 text-green-400">Hello, World</h1>
+            <h1 className="font-bold text-3xl text-blue-dark">Hello, World</h1>
         </>
     );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "blue-dark": "#003d7e",
+                "blue-light": "#007bff",
+            },
+        },
     },
     plugins: [],
 };
