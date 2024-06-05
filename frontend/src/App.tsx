7260d359fcdfdb3bd122c841c39c89ba85b3fdaf
Douglas Henrique <dev.dhbarboza@gmail.com>
2024-06-04 21:39:22 -0300

    feat: Add components Header and Container Page of content

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
+import { Content } from "./components/Content/Content";
+import { HeaderTitle } from "./components/Header/HeaderTitle";
+
 function App() {
     return (
         <>
-            <h1 className="font-bold text-3xl text-blue-dark">Hello, World</h1>
+            <Content>
+                <HeaderTitle />
+                <h1 className="font-bold text-3xl text-blue-dark">
+                    Hello, World
+                </h1>
+            </Content>
         </>
     );
 }
---
+++ frontend/src/components/Content/Content.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+type Props = {
+    children: ReactNode;
+};
+
+export function Content({ children }: Props) {
+    return <div className="px-5 py-4 max-w-7xl mx-auto w-full">{children}</div>;
+}
